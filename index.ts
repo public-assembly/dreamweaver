e75223b4d96618897fadeced1609005d00e34448
Salief <salieflewis@gmail.com>
2023-06-06 10:31:38 -0400

    add funding mechanism

--- index.ts
+++ index.ts
@@ -18,6 +18,27 @@ async function main() {
   );
 
   console.log('Connected wallet address:', bundlr.address);
+
+  /**
+   * Set up upfront funding to the node
+   * Value supplied should be in atomic units
+   *
+   * .25 Ether * 1e18 Wei/Ether = 2.5e17 Wei
+   */
+  const fundTx = await bundlr.fund(2.5e17);
+
+  // Query the your wallet balance on the connected node
+  const atomicBalance = await bundlr.getLoadedBalance();
+
+  const convertedBalance = bundlr.utils.fromAtomic(atomicBalance);
+
+  console.log('Account balance:', convertedBalance);
+
+  // const pathToFile = './images/your_image.png';
+
+  // const uploadTx = await bundlr.uploadFile(pathToFile);
+
+  // console.log('File uploaded:', `https://arweave.net/${uploadTx.id}`);
 }
 
 main();
