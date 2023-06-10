f392ae59d2c43d11476834fb2ce87e229547c0e2
Salief <salieflewis@gmail.com>
2023-06-10 11:00:47 -0400

    handle serializations of bigints, return path to data

--- index.ts
+++ index.ts
@@ -26,25 +26,37 @@ async function main() {
    * Value supplied should be in atomic units
    *
    * .25 Ether * 1e18 Wei/Ether = 2.5e17 Wei
+   *
+   * .1 Ether * 1e18 Wei/Ether 1e17 Wei
    */
-  const fundTx = await bundlr.fund(2.5e17);
+  // const fundTx = await bundlr.fund(1e17);
 
   // Query the your wallet balance on the connected node
   const atomicBalance = await bundlr.getLoadedBalance();
 
-  const convertedBalance = bundlr.utils.fromAtomic(atomicBalance);
+  const convertedBalance = bundlr.utils.fromAtomic(atomicBalance).toString();
 
-  console.log('Account balance:', convertedBalance);
+  // console.log('Account balance:', convertedBalance);
 
-  // const { logs } = await getPressCreationEvents();
+  const logsJson = await getPressCreationEvents();
 
-  // const uploadTx = await bundlr.upload(logs);
+  // console.log('Press creation events:', logs);
+
+  console.log(typeof logsJson);
+
+  const response = await bundlr.upload(logsJson);
+
+  console.log(`File uploaded ==> https://arweave.net/${response.id}`);
+
+  const pathToData = `https://arweave.net/${response.id}`;
 
   // const pathToFile = './images/your_image.png';
 
-  // const uploadTx = await bundlr.uploadFile(pathToFile);
+  // const response = await bundlr.uploadFile(pathToFile);
 
-  // console.log('File uploaded:', `https://arweave.net/${uploadTx.id}`);
+  // console.log('File uploaded:', `https://arweave.net/${response.id}`);
+
+  return pathToData;
 }
 
 main();
