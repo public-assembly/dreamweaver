f33c4725ac5df62ee266b151183c525f219fea41
Salief <salieflewis@gmail.com>
2023-06-13 18:14:46 -0400

    push up press pointer, wip state

--- onchain/src/PressPointer.sol
+++ onchain/src/PressPointer.sol
@@ -3,28 +3,16 @@
 pragma solidity ^0.8.17;
 
 import {ERC721} from "solmate/tokens/ERC721.sol";
+import {IMetadataRenderer} from "./JSONURIRenderer.sol";
 
 contract PressPointer is ERC721 {
-    /// @dev URI prefix used by `tokenURI`
-    string public baseURI;
+    /// @dev Address of the metadata renderer used by `tokenURI`
+    address public metadataRenderer;
 
-    /// @dev Initial resource
-    // string public tokenURI;
-
-    constructor(string memory _name, string memory _symbol, string memory _baseURI) ERC721(_name, _symbol) {
-        baseURI = _baseURI;
-    }
-
-    /// @dev Assigns a new string to `baseURI`
-    function setBaseURI(string memory _newBaseURI) external {
-        baseURI = _newBaseURI;
-    }
-
-    // function setTokenURI(string memory) public {
-
-    // }
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
 
     function tokenURI(uint256 id) public view override returns (string memory) {
-        return string(abi.encodePacked(baseURI, tokenURI));
+        // if (!_exists(id)) revert InvalidTokenId();
+        return IMetadataRenderer(metadataRenderer).tokenURI(id);
     }
 }
