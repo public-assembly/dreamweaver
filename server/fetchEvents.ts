ef3431fc9f045ee12df8fc210ba396d21a3565ef
jawndiego <jawndiego@gmail.com>
2023-07-11 00:11:40 -0400

    code cleanup
    fetchLogs now async function, defined EventObject type, made eventObjects more aesthetic lol. ty johnny for this suggestion.
    
    also moving towards global address type:
    
    type EthereumAddress = Hex;

--- server/fetchEvents.ts
+++ server/fetchEvents.ts
@@ -5,18 +5,21 @@ import Bundlr from '@bundlr-network/client';
 import { ERC721PressFactoryAbi, CurationDatabaseV1Abi } from './abi';
 import { SEPOLIA_ADDRESSES } from './contractAddresses/addresses';
 
+
+type EthereumAddress = Hex;
+
 // Constants
 const ERC721PFABI = ERC721PressFactoryAbi;
-const ERC721_PRESS_FACTORY_ADDRESS = SEPOLIA_ADDRESSES.ERC721.ERC721_PRESS_FACTORY_PROXY as Hex;
+const ERC721_PRESS_FACTORY_ADDRESS = SEPOLIA_ADDRESSES.ERC721.ERC721_PRESS_FACTORY_PROXY as EthereumAddress;
 const CREATE_PRESS_EVENT = 'Create721Press';
-const CURATION_DATABASE_ADDRESS = SEPOLIA_ADDRESSES.ERC721.CURATION_DATABASE_V1 as Hex; 
+const CURATION_DATABASE_ADDRESS = SEPOLIA_ADDRESSES.ERC721.CURATION_DATABASE_V1 as EthereumAddress; 
 const CURATION_V1_ABI = CurationDatabaseV1Abi; 
 const DATA_STORED_EVENT = 'DataStored';
 const RENDER_UPDATED_EVENT = 'RendererUpdated';
 const LOGIC_UPDATED_EVENT = 'LogicUpdated';
 const PRESS_INITIALIZED_EVENT = 'PressInitialized';
 
-// Initialize Bundlr once
+// Initialize Bundlr
 const bundlr = new Bundlr(
   'http://devnet.bundlr.network',
   'ethereum',
@@ -26,7 +29,6 @@ const bundlr = new Bundlr(
   }
 );
 
-// Custom replacer function to handle BigInt serialization
 const replacer = (key: string, value: any) => {
   if (typeof value === 'bigint') {
     return value.toString();
@@ -34,12 +36,23 @@ const replacer = (key: string, value: any) => {
   return value;
 };
 
-async function fetchLogs(fromBlock: bigint, toBlock: bigint, eventNames: string[], abi: any, addresses: Hex[]) {
+const createBundlrTags = (eventName: string) => [
+    { name: 'Content-Type', value: 'application/json' },
+    { name: 'Press Events', value: eventName },
+];
+
+type EventObject = {
+  event: string,
+  abi: any,
+  address: Hex
+}
+
+async function fetchLogs(fromBlock: bigint, toBlock: bigint, eventObjects: EventObject[]) {
   const filters = await Promise.all(
-    addresses.map((address, index) => client.createContractEventFilter({
-      abi: abi[index],
-      address,
-      eventName: eventNames[index],
+    eventObjects.map((eventObject) => client.createContractEventFilter({
+      abi: eventObject.abi,
+      address: eventObject.address,
+      eventName: eventObject.event,
       fromBlock: BigInt(fromBlock),
       toBlock: BigInt(toBlock),
     }))
@@ -49,17 +62,16 @@ async function fetchLogs(fromBlock: bigint, toBlock: bigint, eventNames: string[
 
   // Return a list of event logs since the filter was created
   const logs = await Promise.all(
-    filters.map((filter, index) => client.getFilterLogs({ filter: filter as any }).then(logs => logs.map(log => ({ ...log, eventName: eventNames[index] }))))
+    filters.map((filter, index) => 
+      client.getFilterLogs({ filter: filter as any })
+        .then(logs => logs.map(log => ({ ...log, eventName: eventObjects[index].event }))))
   );
 
   return logs.flat();
 }
 
 async function uploadLog(log: any, eventName: string) {
-  const tags = [
-    { name: 'Content-Type', value: 'application/json' },
-    { name: 'Press Events', value: eventName },
-  ];
+  const tags = createBundlrTags(eventName);
 
   const response = await bundlr.upload(JSON.stringify(log, replacer, 2), { tags });
 
@@ -68,10 +80,7 @@ async function uploadLog(log: any, eventName: string) {
 }
 
 async function uploadLogsGrouped(logs: any[], eventName: string) {
-  const tags = [
-    { name: 'Content-Type', value: 'application/json' },
-    { name: 'Press Events', value: eventName },
-  ];
+  const tags = createBundlrTags(eventName);
 
   const response = await bundlr.upload(JSON.stringify(logs, replacer, 2), { tags });
 
@@ -88,22 +97,18 @@ export async function getPressCreationEvents() {
   let fromBlock = await getLastCheckedBlock();
   const allLogs = [];
 
-  const eventObjects = [
+  const eventObjects: EventObject[] = [
     { event: CREATE_PRESS_EVENT, abi: ERC721PFABI, address: ERC721_PRESS_FACTORY_ADDRESS },
     { event: DATA_STORED_EVENT, abi: CURATION_V1_ABI, address: CURATION_DATABASE_ADDRESS },
     { event: RENDER_UPDATED_EVENT, abi: CURATION_V1_ABI, address: CURATION_DATABASE_ADDRESS },
     { event: LOGIC_UPDATED_EVENT, abi: CURATION_V1_ABI, address: CURATION_DATABASE_ADDRESS },
     { event: PRESS_INITIALIZED_EVENT, abi: CURATION_V1_ABI, address: CURATION_DATABASE_ADDRESS },
-  ];
-
-  const events = eventObjects.map(e => e.event);
-  const abis = eventObjects.map(e => e.abi);
-  const addresses = eventObjects.map(e => e.address);
+];
 
   while (fromBlock <= currentBlock) {
     const toBlock = fromBlock + BigInt(1);
 
-    const logs = await fetchLogs(fromBlock, toBlock, events, abis, addresses);
+    const logs = await fetchLogs(fromBlock, toBlock, eventObjects);
     
     // Separate CreatePress logs and other logs
     const createPressLogs = logs.filter(log => log.eventName === CREATE_PRESS_EVENT);
