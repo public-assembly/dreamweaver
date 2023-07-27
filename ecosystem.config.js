a792433eb8a97e4e8c6880a2fa87a1ff82904e9e
Salief <salieflewis@gmail.com>
2023-07-27 16:43:28 -0400

    include interpreter in pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
       name: "dreamweaver",
       script: "./processAndUpload.ts",
       instances: 1,
+      interpreter: "ts-node",
       env: {
         NODE_ENV: "development",
       },
